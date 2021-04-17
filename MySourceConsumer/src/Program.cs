b107214b62ba45046d8194155feb3410270ae686
Laicasaane <laicasaane@outlook.com>
2021-04-17 17:34:23 +0700

    Add Union example

--- MySourceConsumer/src/Program.cs
+++ MySourceConsumer/src/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Runtime.InteropServices;
 using MyLibrary;
 
 namespace MySourceConsumer
@@ -10,7 +11,7 @@ namespace MySourceConsumer
         static void Main(string[] args)
         {
             var vIF = new IntFloat();
-            vIF.Float = 4f;
+            vIF.Float = 5f;
         }
     }
 
@@ -20,9 +21,68 @@ namespace MySourceConsumer
 
 namespace Examples
 {
-    public struct Union
+    [StructLayout(LayoutKind.Explicit, Pack = 1)]
+    public partial struct Union
     {
-        public int Int;
-        public float Float;
+        public enum Type : byte
+        {
+            Int,
+            Float,
+            Long,
+        }
+
+        [FieldOffset(0)]
+        public readonly Type ValueType;
+
+        [FieldOffset(1)]
+        public readonly int Int;
+
+        [FieldOffset(1)]
+        public readonly float Float;
+
+        [FieldOffset(1)]
+        public readonly long Long;
+
+        public Union(int value)
+        {
+            this.ValueType = Type.Int;
+            this.Float = default;
+            this.Long = default;
+            this.Int = value;
+        }
+
+        public Union(float value)
+        {
+            this.ValueType = Type.Float;
+            this.Int = default;
+            this.Long = default;
+            this.Float = value;
+        }
+
+        public Union(long value)
+        {
+            this.ValueType = Type.Long;
+            this.Int = default;
+            this.Float = default;
+            this.Long = value;
+        }
+
+        public static implicit operator Union(int value)
+            => new(value);
+
+        public static implicit operator Union(float value)
+            => new(value);
+
+        public static implicit operator Union(long value)
+            => new(value);
+
+        public static implicit operator int(in Union value)
+            => value.Int;
+
+        public static implicit operator float(in Union value)
+            => value.Float;
+
+        public static implicit operator long(in Union value)
+            => value.Long;
     }
 }
